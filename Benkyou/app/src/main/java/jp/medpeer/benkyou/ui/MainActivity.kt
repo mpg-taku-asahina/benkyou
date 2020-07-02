2ddc49395ea211b277559872464b81af9d6ecb05
朝比奈 拓 <taku.asahina@medpeer.co.jp>
2020-07-02 15:57:51 +0900

    電卓の数字キーを押したらを実装

--- Benkyou/app/src/main/java/jp/medpeer/benkyou/ui/MainActivity.kt
+++ Benkyou/app/src/main/java/jp/medpeer/benkyou/ui/MainActivity.kt
@@ -3,15 +3,28 @@ package jp.medpeer.benkyou.ui
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
+import androidx.databinding.ObservableField
 import jp.medpeer.benkyou.R
 import jp.medpeer.benkyou.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    val number: ObservableField<String> = ObservableField("0")
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        val binding: ActivityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        val binding: ActivityMainBinding =
+            DataBindingUtil.setContentView(this, R.layout.activity_main)
         binding.view = this
+    }
+
+    fun selectNumber(key: Int) {
+        if (number.get() == "0") {
+            number.set("")
+        }
+
+        val aa = number.get() + key.toString()
+        number.set(aa)
     }
 }
